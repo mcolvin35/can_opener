194a42c4bcfbae67ccd4b1ce632e155c6c7b2286
mcolvin35 <mcolvin35@charlottesvilleschools.org>
2023-03-07 11:32:13 -0500

    beans

--- can_opener.py
+++ can_opener.py
@@ -17,9 +17,10 @@ while True:
     if cur_state != prev_state: #if cur_state is different than prev_state
         if not cur_state:
             print("BTN1 is up")
-                 myservo.throttle = -1.0
+            myservo.throttle = -1.0
         else:
             print("BTN1 is down")
-                 my_servo.throttle = 1.0 #servo moves 180 degrees in one direction
+            my_servo.throttle = 1.0 #servo moves 180 degrees in one direction
 
 prev_state = cur_state #sets prev_state to cur_state
+#hello
