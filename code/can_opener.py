c5958f10fa440e6a1c9b83623abe26fa9cc31965
mcolvin35 <mcolvin35@charlottesvilleschools.org>
2023-03-07 11:54:45 -0500

    added ignore to stop error messages

--- code/can_opener.py
+++ code/can_opener.py
@@ -1,3 +1,4 @@
+#type: ignore
 import board
 from digitalio import DigitalInOut, Direction, Pull
 import pwmio
@@ -5,7 +6,7 @@ from adafruit_motor import servo
 
 
 pwm = pwmio.PWMOut(board.D13, duty_cycle=2 ** 15, frequency= 100)
-my_servo = servo = servo.ContinuousServo(pwm, min_pulse=500, max_pulse=2600)
+my_servo = servo.ContinuousServo(pwm, min_pulse=500, max_pulse=2600)
 btn = DigitalInOut(board.D12) #links button 1 to board
 btn.direction = Direction.INPUT #btn1 is input
 btn.pull = Pull.DOWN #recognize input when button is pressed
@@ -17,7 +18,7 @@ while True:
     if cur_state != prev_state: #if cur_state is different than prev_state
         if not cur_state:
             print("BTN1 is up")
-            myservo.throttle = -1.0
+            my_servo.throttle = -1.0
         else:
             print("BTN1 is down")
             my_servo.throttle = 1.0 #servo moves 180 degrees in one direction
