d4fa64e2341e35aaeac17bd2bc1df1c973b4a443
mcolvin35 <mcolvin35@charlottesvilleschools.org>
2023-03-10 11:16:56 -0500

    final version

--- code/canOpenerComp.py
+++ code/canOpenerComp.py
@@ -1,16 +1,17 @@
 import time
 import digitalio as DIo
-from adafruit_motor import stepper,servo
+import pwmio
+import servo
 import board
 from analogio import AnalogIn, AnalogOut 
 
-btn = DIo.DigitalInOut(board.D5)
+btn = DIo.DigitalInOut(board.D12)
 btn.direction = DIo.Direction.INPUT
 btn.pull = DIo.Pull.DOWN
 
 pwm = pwmio.PWMOut(board.D13, duty_cycle=2 ** 15, frequency= 100)
 my_servo = servo.ContinuousServo(pwm, min_pulse=500, max_pulse=2600)
-
+my_servo.throttle = 0
 prevState = 0     
 curState = 0  
 savedTime =0
@@ -32,11 +33,13 @@ def btncall(btnVal):
         if savedTime != 0:
             my_servo.throttle = 1
     elif btnVal == False:
+        my_servo.throttle = 0
+        print("off")
         prevState = False
     print(f"{savedTime} {prevState} {btnVal}")
 
 
 while True:
-    time.sleep(1)
+    time.sleep(.01)
     btncall(btn.value)
     
